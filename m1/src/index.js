74071347ea5553dc9e97c7b6871c1109c4706a67
deger4beger <deger.begerrr@gmail.com>
2023-08-05 20:12:34 +0600

    feat: try/catch +

--- m1/src/index.js
+++ m1/src/index.js
@@ -11,29 +11,32 @@ const REQUEST_QUEUE = "request";
 const RESPONSE_QUEUE = "response";
 
 fastify.post('/', async function handler (request, reply) {
+  try {
+    const channel = await this.amqp.createChannel();
+    await channel.assertQueue(REQUEST_QUEUE);
+    await channel.assertQueue(RESPONSE_QUEUE);
 
-  const channel = await this.amqp.createChannel();
-  await channel.assertQueue(REQUEST_QUEUE);
-  await channel.assertQueue(RESPONSE_QUEUE);
+    const correlationId = uuidv4();
 
-  const correlationId = uuidv4();
+    await channel.sendToQueue(
+      REQUEST_QUEUE,
+      Buffer.from(JSON.stringify(request.body)),
+      { correlationId }
+    );
 
-  await channel.sendToQueue(
-    REQUEST_QUEUE,
-    Buffer.from(JSON.stringify(request.body)),
-    { correlationId }
-  );
+    await channel.consume(RESPONSE_QUEUE, async (msg) => {
+      if (correlationId !== msg.properties.correlationId) return;
 
-  await channel.consume(RESPONSE_QUEUE, async (msg) => {
-    if (correlationId !== msg.properties.correlationId) return;
+      const parsedMsg = JSON.parse(msg.content.toString());
+      reply.send(parsedMsg)
 
-    const parsedMsg = JSON.parse(msg.content.toString());
-    reply.send(parsedMsg)
+      await channel.close();
+    }, { noAck: true });
 
-    await channel.close();
-  }, { noAck: true });
-
-  await reply;
+    await reply;
+  } catch {
+    reply.code(500).send({ msg: "Some server error occured" })
+  }
 })
 
 try {
