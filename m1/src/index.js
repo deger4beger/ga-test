a2a539b298756d19399b6a42c51ec1e72761b132
deger4beger <deger.begerrr@gmail.com>
2023-08-05 18:51:52 +0600

    feat: base rabbit setup

--- m1/src/index.js
+++ m1/src/index.js
@@ -1,17 +1,46 @@
 import Fastify from 'fastify';
+import amqp from "amqplib";
 
 
 const fastify = Fastify({
   logger: true
 })
+const REQUEST_QUEUE = "request";
+const RESPONSE_QUEUE = "response";
 
 fastify.get('/', async function handler (request, reply) {
-  return { hello: 'Hello from m1 microservice !' };
+
+  const channel = await this.amqp.createChannel();
+  await channel.assertQueue(REQUEST_QUEUE);
+  await channel.assertQueue(RESPONSE_QUEUE);
+
+  await channel.sendToQueue(REQUEST_QUEUE, Buffer.from("some_task"))
+  console.log('MSG sent');
+
+  await channel.consume(RESPONSE_QUEUE, async (msg) => {
+    const textMsg = msg.content.toString();
+    console.log('MSG recieved', textMsg);
+    channel.ack(msg);
+
+    reply.send({ rabbit: textMsg })
+    await channel.close();
+  }, { noAck: false });
+
+  return reply;
 })
 
 try {
+
+  await (async () => {
+    const connection = await amqp.connect("amqp://admin:admin@rabbitmq:5672");
+    fastify.decorate("amqp", connection);
+  })();
+
   await fastify.listen({ port: 8000, host: "0.0.0.0" });
 } catch (err) {
+
+  await fastify.amqp.close();
+
   fastify.log.error(err);
   process.exit(1);
 }
--- m2/src/index.js
+++ m2/src/index.js
@@ -1,17 +1,35 @@
 import Fastify from 'fastify';
+import amqp from "amqplib";
 
 
 const fastify = Fastify({
   logger: true
 })
-
-fastify.get('/', async function handler (request, reply) {
-  return { hello: 'Hello from m2 microservice !' };
-})
+const REQUEST_QUEUE = "request";
+const RESPONSE_QUEUE = "response";
 
 try {
+  await (async () => {
+    const connection = await amqp.connect("amqp://admin:admin@rabbitmq:5672");
+    fastify.decorate("amqp", connection);
+  })();
   await fastify.listen({ port: 8000, host: "0.0.0.0" });
+
+
+  const channel = await fastify.amqp.createChannel();
+  await channel.assertQueue(REQUEST_QUEUE);
+  await channel.assertQueue(RESPONSE_QUEUE);
+
+  await channel.consume(REQUEST_QUEUE, async (msg) => {
+    const textMsg = msg.content.toString();
+    console.log('MSG recieved', textMsg);
+    channel.ack(msg);
+    await channel.sendToQueue(RESPONSE_QUEUE, Buffer.from(textMsg))
+    console.log('MSG sent');
+  }, { noAck: false });
+
 } catch (err) {
+  await fastify.amqp.close();
   fastify.log.error(err);
   process.exit(1);
 }
